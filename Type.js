fa504357bed8cd2626665a3ca178b32107d425f7
Dickson Edung <dicksoneedung@gmail.com>
2022-05-06 22:18:14 +0100

    V 2.0
    Updated typewriting effect with If statements and loops

--- Type.js
+++ Type.js
@@ -1,14 +1,51 @@
-    const textDisplay = document.getElementById('text')
-    const phrases = ["of your offerings", "or your tithes", "or the shekels you have to pay"]
+const textDisplay = document.getElementById('text')
+const phrases = ['of your offerings', 'of your tithes', 'or shekels to give']
+let i = 0
+let j = 0 
+let currentPhrase = []
+let isDeleting = false
+let isEnd = false
 
-    function loop () {
+function loop () {
+  isEnd = false
+  textDisplay.innerHTML = currentPhrase.join('')
 
-        if (i < phrases.length) {
-            console.log(phrases[i])
-            if (j < phrases[i].length)
-            console.log(phrases[i][j])
-        }
+  if (i < phrases.length) {
+
+    if (!isDeleting && j <= phrases[i].length) {
+      currentPhrase.push(phrases[i][j])
+      j++
+      textDisplay.innerHTML = currentPhrase.join('')
     }
+
+    if(isDeleting && j <= phrases[i].length) {
+      currentPhrase.pop(phrases[i][j])
+      j--
+      textDisplay.innerHTML = currentPhrase.join('')
+    }
+
+    if (j == phrases[i].length) {
+      isEnd = true
+      isDeleting = true
+    }
+
+    if (isDeleting && j === 0) {
+      currentPhrase = []
+      isDeleting = false
+      i++
+      if (i === phrases.length) {
+        i = 0
+      }
+    }
+  }
+  const spedUp = Math.random() * (80 -50) + 50
+  const normalSpeed = Math.random() * (300 -200) + 200
+  const time = isEnd ? 2000 : isDeleting ? spedUp : normalSpeed
+  setTimeout(loop, time)
+}
+
+loop()
+
 
         function myFunction() {
             var Gender = document.getElementById("gender").value;
